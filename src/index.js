19c6dfc6286d3dce3d5423e1ba298a5924283ae8
aksr666 <aksr100793@mail.ru>
2019-02-20 17:06:24 +0300

    triangles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+module.exports = getLoveTrianglesCount = preferences => preferences.filter((value, index, array) => { 
+  let a = value; 
+  let b = array[a - 1]; 
+  let c = array[b - 1]
+  return c == index + 1 && a !== b && b !== c;
+}).length / 3;
