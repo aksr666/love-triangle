b8a46438ca188f4843218532947892cb639f0596
aksr666 <47743186+aksr666@users.noreply.github.com>
2019-03-14 00:20:35 +0300

    refactor: change solution

--- src/index.js
+++ src/index.js
@@ -2,9 +2,4 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-module.exports = getLoveTrianglesCount = preferences => preferences.filter((value, index, array) => { 
-  let a = value; 
-  let b = array[a - 1]; 
-  let c = array[b - 1]
-  return c == index + 1 && a !== b && b !== c;
-}).length / 3;
+module.exports=getLoveTrianglesCount=preferences=>preferences.filter((value,index,array)=>array[array[value-1]-1]==index+1).length/3;
